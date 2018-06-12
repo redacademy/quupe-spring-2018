a7cabdd9c780276b64b7a091fff475aa6e7f5a38
Dakota <dakota.paige@hotmail.com>
2018-06-12 14:25:27 -0700

    Added firebase to ios files and started roughing out containers/components for the sign in page.

---
+++ quupe-RN/js/screens/SignIn/SignInContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import SignIn from './SignIn';
+
+class SignInContainer extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <SignIn />
+    )
+  }
+}
+
+export default SignInContainer;
+
