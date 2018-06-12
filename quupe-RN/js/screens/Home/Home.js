c3741e307f7d0f26dd1585e77c54c6782936152d
Ryan Cs. Ku <ryancsku@gmail.com>
2018-06-12 16:15:34 -0700

    Updated and uncommented Home file.

--- quupe-RN/js/screens/Home/Home.js
+++ quupe-RN/js/screens/Home/Home.js
@@ -1,15 +1,15 @@
-// import React, { Component } from 'react';
-// import { Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 
-// import PropTypes from 'prop-types';
-// import styles from './styles';
+import PropTypes from 'prop-types';
+import styles from './styles';
 
-// const Home = props => {
-//   return (
-//     <View>
-//       <Text>Text Insert</Text>
-//     </View>
-//   );
-// };
+const Home = props => {
+  return (
+    <View>
+      <Text>Text Insert</Text>
+    </View>
+  );
+};
 
-// export default Home;
+export default Home;
