be45acfc7798a9da26dd68ce0bc0127efa92ec52
Brandon Liu <brandon.slipjet@gmail.com>
2018-06-13 09:06:13 -0700

    var removed, commented code removed, preset env added.

--- quupe-server/src/api/schema.js
+++ quupe-server/src/api/schema.js
@@ -1,30 +1,3 @@
-// const typeDefs = `{
-// type Item{
-//      id: ID!
-//      title: String!
-//      details: String!
-//      geolocation: String!
-//      image: String!
-//      condition: String!
-//      year: String!
-//      categories: [String!]
-//      price: String!
-//      available: Boolean
-//      }
-// type Category{
-//      categoryid: ID
-//      category: String!
-// }
-// type Query {
-//      items: [Item]
-//      item(id: ID!): Item
-//      categoryScreen: [Category]
-// }
-
-// }`;
-
-// export default typeDefs;
-
 export default `
 type Item{
      _id: String!
--- quupe-server/src/index.js
+++ quupe-server/src/index.js
@@ -18,7 +18,7 @@ const schema = makeExecutableSchema({
 });
 
 mongoose.connect('mongodb://localhost/test');
-var db = mongoose.connection;
+let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
     console.log('were connection');
