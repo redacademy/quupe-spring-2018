88f40f5520a641c4d9d868cc11300cfae3bb68f9
Dakota <dakota.paige@hotmail.com>
2018-06-17 14:48:42 -0700

    Sign up user is now adding user, adding id and token to realm and redirecting to home page after sign up

--- quupe-RN/js/screens/SignUp/SignUpContainer.js
+++ quupe-RN/js/screens/SignUp/SignUpContainer.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { Mutation } from 'react-apollo';
+import { connect } from 'react-redux';
 import SignUp from './SignUp';
+import { createUserToken } from '../../redux/modules/Token';
 
 const signUpMutation = gql`
   mutation signupUser(
@@ -21,11 +23,16 @@ const signUpMutation = gql`
 `;
 
 class SignUpContainer extends Component {
+  saveAuthToken(token, id) {
+    this.props.dispatch(createUserToken(token, id));
+    this.props.navigation.navigate('Home');
+  }
 
   render() {
     return (
       <Mutation
         mutation={signUpMutation}
+        onCompleted={(data) => this.saveAuthToken(data.signupUser.token, data.signupUser.id)}
       >
         {(signupUser, { data, loading, error }) => (
           <SignUp
@@ -40,4 +47,4 @@ class SignUpContainer extends Component {
   }
 }
 
-export default SignUpContainer;
+export default connect()(SignUpContainer);
