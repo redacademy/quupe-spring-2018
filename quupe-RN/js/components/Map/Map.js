7c9c6cbc666522e28d69a80329e304b7ad1f3c26
Eric Chan <ericchan277@gmail.com>
2018-06-18 13:03:10 -0700

    Updated proptype to be required

--- quupe-RN/js/components/Map/Map.js
+++ quupe-RN/js/components/Map/Map.js
@@ -9,30 +9,31 @@ import styles from './styles';
 const Map = ({ location }) => (
     <View style={styles.mapContainer}>
         {location.latitude &&
-                location.longitude && (
+            location.longitude && (
             <MapView
-                    style={styles.map}
-                    region={{
+                style={styles.map}
+                region={{
                     latitude: location.latitude,
                     longitude: location.longitude,
                     latitudeDelta: 0.3,
                     longitudeDelta: 0.3
                 }}
-                >
-                    <MapView.Marker
+            >
+                <MapView.Marker
                     coordinate={{
                         latitude: location.latitude,
                         longitude: location.longitude
                     }}
                     title="Your Location"
                 />
-                </MapView>
+            </MapView>
         )}
     </View>
 );
 
 Map.propTypes = {
-    location: PropTypes.object.isRequired
+    location: PropTypes.objectOf(PropTypes.oneOfType([PropTypes.object]))
+        .isRequired
 };
 
 export default Map;
