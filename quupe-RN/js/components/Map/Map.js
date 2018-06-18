f31efcf834995e7910b3da7ba793585f2eccf01a
Eric Chan <ericchan277@gmail.com>
2018-06-18 12:02:42 -0700

    Fixed PropType Validation

--- quupe-RN/js/components/Map/Map.js
+++ quupe-RN/js/components/Map/Map.js
@@ -6,35 +6,33 @@ import PropTypes from 'prop-types';
 
 import styles from './styles';
 
-const Map = ({ location }) => {
-  return (
+const Map = ({ location }) => (
     <View style={styles.mapContainer}>
-      {location.latitude &&
-        location.longitude && (
-          <MapView
-            style={styles.map}
-            region={{
-              latitude: location.latitude,
-              longitude: location.longitude,
-              latitudeDelta: 0.3,
-              longitudeDelta: 0.3
-            }}
-          >
-            <MapView.Marker
-              coordinate={{
-                latitude: location.latitude,
-                longitude: location.longitude
-              }}
-              title={'Your Location'}
-            />
-          </MapView>
+        {location.latitude &&
+                location.longitude && (
+            <MapView
+                    style={styles.map}
+                    region={{
+                    latitude: location.latitude,
+                    longitude: location.longitude,
+                    latitudeDelta: 0.3,
+                    longitudeDelta: 0.3
+                }}
+                >
+                    <MapView.Marker
+                    coordinate={{
+                        latitude: location.latitude,
+                        longitude: location.longitude
+                    }}
+                    title="Your Location"
+                />
+                </MapView>
         )}
     </View>
-  );
-};
+);
 
 Map.propTypes = {
-  location: PropTypes.number
+    location: PropTypes.object.isRequired
 };
 
 export default Map;
